41a37530f32d055a85efd3b3fd2c1e0a7352679a
Cristiano Pedro <145721543+Cristiano67867@users.noreply.github.com>
2025-12-24 18:10:06 -0300

    Getenv 4

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-	"log"
-	"os"
-	"net/http"
-	"github.com/cristiano_code/qrcode/handlers"
+    "log"
+    "os"
+    "net/http"
+    "github.com/cristiano_code/qrcode/handlers"
 )
 
 func main() {
-	fileServerTemplates := http.FileServer(http.Dir("./templates"))
-	fileServerStatic := http.FileServer(http.Dir("./static"))
+    fileServerTemplates := http.FileServer(http.Dir("./templates"))
+    fileServerStatic := http.FileServer(http.Dir("./static"))
 
-	http.Handle("/", fileServerTemplates)
-	http.Handle("/static/", http.StripPrefix("/static/", fileServerStatic))
+    http.Handle("/", fileServerTemplates)
+    http.Handle("/static/", http.StripPrefix("/static/", fileServerStatic))
 
-	http.HandleFunc("/qrcode", handlers.GenarateQrcode)
+    // Corrigir nome da função
+    http.HandleFunc("/qrcode", handlers.GenerateQrcode)
 
-	log.Print("Server running in :8080")
-	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    log.Printf("Server running on port %s", port)
+    http.ListenAndServe(":"+port, nil)
 }
